1ec7dbbddc6f98b65e4ba7fc796f6b38f530ad3e
riham5477 <golamshahrearereham12@gmail.com>
2026-08-09 16:03:47 +0600

    implement dynamically display all plants

--- script.js
+++ script.js
@@ -16,3 +16,47 @@ const displayCategories = (categories) => {
     categoriesContainer.appendChild(categoryList);
   }
 };
+const loadAllPlants = () => {
+  fetch("https://openapi.programming-hero.com/api/plants")
+    .then((res) => res.json())
+    .then((json) => displayAllPlants(json.plants));
+};
+loadAllPlants();
+const displayAllPlants = (plants) => {
+  const cardsContainer = document.getElementById("cards-container");
+  cardsContainer.innerHTML = "";
+
+  for (const plant of plants) {
+    const cardDiv = document.createElement("div");
+    cardDiv.innerHTML = `
+        <div class="card bg-base-100 shadow-sm p-4 h-full flex flex-col">
+            <figure class="">
+                <img
+                src="${plant.image}"
+                alt="Plant"
+                class="rounded-xl w-full h-56 object-cover" 
+                />
+            </figure>
+        
+            <div class="items-center flex flex-col flex-grow">
+                <div class="my-3 flex-grow w-full">
+                <h2 class="card-title">${plant.name}</h2>
+                <p>
+                    ${plant.description}
+                </p>
+                <div class="flex justify-between items-center mt-2">
+                    <p class="btn bg-[#DCFCE7] text-[#15803D] rounded-3xl border-none">
+                    ${plant.category}
+                    </p>
+                    <p class="font-bold text-lg">$<span>${plant.price}</span></p>
+                </div>
+                </div>
+                <button class="btn btn-primary w-full rounded-3xl bg-[#15803D] border-none mt-auto">
+                Add to Cart
+                </button>
+            </div>
+        </div>
+        `;
+    cardsContainer.appendChild(cardDiv);
+  }
+};
